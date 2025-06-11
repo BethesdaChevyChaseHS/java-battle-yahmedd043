ec3293839e9d98fc0aa5dabe09921c6d12eafe23
yahmedd043 <yahmedd043@gmail.com>
2025-06-11 22:56:49 +0000

    Final Submission (maybe)

--- app/src/main/java/bcc/javaJostle/MyRobot.java
+++ app/src/main/java/bcc/javaJostle/MyRobot.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 
 public class MyRobot extends Robot{
     public MyRobot(int x, int y){
-        super(x, y, 4, 1, 2, 3,"Robob", "myRobotImage.png", "defaultProjectileImage.png");
-        // Health: 4, Speed: 2, Attack Speed: 3, Projectile Strength: 1
+        super(x, y, 4, 2, 1, 3,"Robob", "robob.png", "defaultProjectileImage.png");
+        // Health: 4, Speed: 2, Attack Speed: 1, Projectile Strength: 3
         // Total = 10
-        // Image name is "myRobotImage.png"
+        // Image name is "robob.png"
     }
 
     public void think(ArrayList<Robot> robots, ArrayList<Projectile> projectiles, Map map, ArrayList<PowerUp> powerups) {
@@ -29,8 +29,8 @@ public class MyRobot extends Robot{
             move(targetRobot, map);
             if(canAttack())
             {
-                // shootAtLocation(targetRobot.getX() + Utilities.ROBOT_SIZE / 2, targetRobot.getY() + Utilities.ROBOT_SIZE / 2);
-                sprayShoot(targetRobot);
+                shootAtLocation(targetRobot.getX() + Utilities.ROBOT_SIZE / 2, targetRobot.getY() + Utilities.ROBOT_SIZE / 2);
+                // sprayShoot(targetRobot);
             }
         }
     }
@@ -63,48 +63,65 @@ public class MyRobot extends Robot{
         double dy = Math.abs(y2 - y1);
 
         int[][] tiles = map.getTiles();
-        int tileLeft = getOccupiedTiles()[0];
-        int tileRight = getOccupiedTiles()[1];
-        int tileTop = getOccupiedTiles()[2];
-        int tileBottom = getOccupiedTiles()[3];
+        int[] occupiedTiles = getOccupiedTiles();
+
+        int tileLeft = occupiedTiles[0];
+        int tileRight = occupiedTiles[1];
+        int tileTop = occupiedTiles[2];
+        int tileBottom = occupiedTiles[3];
+
+        int rows = tiles.length;
+        int cols = tiles[0].length;
 
         if (dx >= dy) {
             if (x1 < x2) { // move right
-                if (tiles[tileTop][tileRight + 1] != Utilities.WALL && tiles[tileBottom][tileRight + 1] != Utilities.WALL &&
+                if (tileRight + 1 < cols &&
+                    tiles[tileTop][tileRight + 1] != Utilities.WALL && tiles[tileBottom][tileRight + 1] != Utilities.WALL &&
                     tiles[tileTop][tileRight + 1] != Utilities.MUD && tiles[tileBottom][tileRight + 1] != Utilities.MUD) {
                     xMovement = 1;
-                } else if (tiles[tileTop - 1][tileLeft] != Utilities.WALL && tiles[tileLeft][tileTop - 1] != Utilities.MUD) {
+                } else if (tileTop - 1 >= 0 &&
+                           tiles[tileTop - 1][tileLeft] != Utilities.WALL && tiles[tileTop - 1][tileLeft] != Utilities.MUD) {
                     yMovement = -1; // if right is blocked, try moving up
-                } else if (tiles[tileBottom + 1][tileLeft] != Utilities.WALL && tiles[tileLeft][tileBottom + 1] != Utilities.MUD) {
+                } else if (tileBottom + 1 < rows &&
+                           tiles[tileBottom + 1][tileLeft] != Utilities.WALL && tiles[tileBottom + 1][tileLeft] != Utilities.MUD) {
                     yMovement = 1; // if both right and up are blocked, try moving down
                 }
             } else { // move left
-                if (tiles[tileTop][tileLeft - 1] != Utilities.WALL && tiles[tileBottom][tileLeft - 1] != Utilities.WALL &&
+                if (tileLeft - 1 >= 0 &&
+                    tiles[tileTop][tileLeft - 1] != Utilities.WALL && tiles[tileBottom][tileLeft - 1] != Utilities.WALL &&
                     tiles[tileTop][tileLeft - 1] != Utilities.MUD && tiles[tileBottom][tileLeft - 1] != Utilities.MUD) {
                     xMovement = -1;
-                } else if (tiles[tileTop - 1][tileLeft] != Utilities.WALL && tiles[tileLeft][tileTop - 1] != Utilities.MUD) {
+                } else if (tileTop - 1 >= 0 &&
+                           tiles[tileTop - 1][tileLeft] != Utilities.WALL && tiles[tileTop - 1][tileLeft] != Utilities.MUD) {
                     yMovement = -1; // if left is blocked, try moving up
-                } else if (tiles[tileBottom + 1][tileLeft] != Utilities.WALL && tiles[tileLeft][tileBottom + 1] != Utilities.MUD) {
+                } else if (tileBottom + 1 < rows &&
+                           tiles[tileBottom + 1][tileLeft] != Utilities.WALL && tiles[tileBottom + 1][tileLeft] != Utilities.MUD) {
                     yMovement = 1; // if both left and up are blocked, try moving down
                 }
             }
         } else {
             if (y1 < y2) { // move down
-                if (tiles[tileBottom + 1][tileLeft] != Utilities.WALL && tiles[tileBottom + 1][tileRight] != Utilities.WALL &&
+                if (tileBottom + 1 < rows &&
+                    tiles[tileBottom + 1][tileLeft] != Utilities.WALL && tiles[tileBottom + 1][tileRight] != Utilities.WALL &&
                     tiles[tileBottom + 1][tileLeft] != Utilities.MUD && tiles[tileBottom + 1][tileRight] != Utilities.MUD) {
                     yMovement = 1;
-                } else if (tiles[tileTop][tileRight + 1] != Utilities.WALL && tiles[tileTop][tileRight + 1] != Utilities.MUD) {
+                } else if (tileRight + 1 < cols &&
+                           tiles[tileTop][tileRight + 1] != Utilities.WALL && tiles[tileTop][tileRight + 1] != Utilities.MUD) {
                     xMovement = 1; // if down is blocked, try moving right
-                } else if (tiles[tileTop][tileLeft - 1] != Utilities.WALL && tiles[tileTop][tileLeft - 1] != Utilities.MUD) {
+                } else if (tileLeft - 1 >= 0 &&
+                           tiles[tileTop][tileLeft - 1] != Utilities.WALL && tiles[tileTop][tileLeft - 1] != Utilities.MUD) {
                     xMovement = -1; // if both down and right are blocked, try moving left
                 }
             } else { // move up
-                if (tiles[tileTop - 1][tileLeft] != Utilities.WALL && tiles[tileTop - 1][tileRight] != Utilities.WALL &&
+                if (tileTop - 1 >= 0 &&
+                    tiles[tileTop - 1][tileLeft] != Utilities.WALL && tiles[tileTop - 1][tileRight] != Utilities.WALL &&
                     tiles[tileTop - 1][tileLeft] != Utilities.MUD && tiles[tileTop - 1][tileRight] != Utilities.MUD) {
                     yMovement = -1;
-                } else if (tiles[tileTop][tileRight + 1] != Utilities.WALL && tiles[tileTop][tileRight + 1] != Utilities.MUD) {
+                } else if (tileRight + 1 < cols &&
+                           tiles[tileTop][tileRight + 1] != Utilities.WALL && tiles[tileTop][tileRight + 1] != Utilities.MUD) {
                     xMovement = 1; // if up is blocked, try moving right
-                } else if (tiles[tileTop][tileLeft - 1] != Utilities.WALL && tiles[tileTop][tileLeft - 1] != Utilities.MUD) {
+                } else if (tileLeft - 1 >= 0 &&
+                           tiles[tileTop][tileLeft - 1] != Utilities.WALL && tiles[tileTop][tileLeft - 1] != Utilities.MUD) {
                     xMovement = -1; // if both up and right are blocked, try moving left
                 }
             }
@@ -131,7 +148,7 @@ public class MyRobot extends Robot{
         int distanceX = (getX() + Utilities.ROBOT_SIZE / 2) - (targetRobot.getX() + Utilities.ROBOT_SIZE / 2);
         int distanceY = (getY() + Utilities.ROBOT_SIZE / 2) - (targetRobot.getY() + Utilities.ROBOT_SIZE / 2);
         double angle = Math.atan2(distanceY, distanceX);
-        double offset = (Math.random() * 15) - 7.5;
+        double offset = (Math.random() * 7.5) - 3.25;
         double distance = Math.hypot(getX() - targetRobot.getX(), getY() - targetRobot.getY());
         shootAtLocation(getX() + Utilities.ROBOT_SIZE / 2 - (int) (Math.cos(angle + (Math.PI /180 * offset)) * distance), getY() + Utilities.ROBOT_SIZE / 2 - (int) (Math.sin(angle + (Math.PI / 180 * offset)) * distance)); 
     }
